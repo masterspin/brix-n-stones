082795a5680ba44a8fbeb66d2d9ef047ecce962e
rjutur <rjutur@umich.edu>
2023-08-07 16:41:12 -0400

    added firebase db and stroage linking

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -1,8 +1,49 @@
-import { useEffect } from "react";
+'use client';
+import { useState, useEffect } from "react";
 import Header from "@/components/header";
 import Projects from "@/components/projects";
+import { db, storageRef } from '../firebase'
+import { collection, getDocs, onSnapshot, QuerySnapshot, query } from "firebase/firestore";
+
+async function getProjects(){
+    const querySnapshot = await getDocs(collection(db, "projects"));
+    querySnapshot.forEach((doc) => {
+      // doc.data() is never undefined for query doc snapshots
+      console.log(doc.id, " => ", doc.data());
+    });
+
+    return querySnapshot;
+}
 
 export default function Home() {
+
+  const[projects, setProjects] = useState([] as any);
+  const [projectids, setProjectids] = useState([] as any)
+
+  useEffect(() => {
+    async function getProjects(){
+      const querySnapshot = await getDocs(collection(db, "projects"));
+
+      var nameArr:any[] = []
+      var idArr:any[] = []
+
+      querySnapshot.forEach((doc) => {
+        // doc.data() is never undefined for query doc snapshots
+        nameArr.push(doc.data()['name']);
+        idArr.push(doc.data()['project_id']);
+
+        setProjects(nameArr);
+        setProjectids(idArr);
+
+      });
+
+    }
+    getProjects()
+  },[])
+
+  console.log(projects)
+  console.log(projectids)
+
   return (
     <div className="">
       <Header />
